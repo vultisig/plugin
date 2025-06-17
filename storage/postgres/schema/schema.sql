5460a542f4018f16262930a1ffcb4a26149d4d8a
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-06-18 00:55:23 +0300

    IsAlreadyProposed

--- storage/postgres/schema/schema.sql
+++ storage/postgres/schema/schema.sql
@@ -3,7 +3,7 @@ CREATE TYPE "plugin_id" AS ENUM (
     'vultisig-dca-0000',
     'vultisig-payroll-0000',
     'vultisig-fees-feee'
-);
+    );
 
 CREATE TYPE "transaction_status" AS ENUM (
     'PENDING',
@@ -12,49 +12,49 @@ CREATE TYPE "transaction_status" AS ENUM (
     'BROADCAST',
     'MINED',
     'REJECTED'
-);
+    );
 
 CREATE TYPE "trigger_status" AS ENUM (
     'PENDING',
     'RUNNING'
-);
+    );
 
 CREATE TYPE "tx_indexer_status" AS ENUM (
     'PROPOSED',
     'VERIFIED',
     'SIGNED'
-);
+    );
 
 CREATE TYPE "tx_indexer_status_onchain" AS ENUM (
     'PENDING',
     'SUCCESS',
     'FAIL'
-);
+    );
 
 CREATE TABLE "plugin_policies" (
-    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
-    "public_key" "text" NOT NULL,
-    "plugin_id" "plugin_id" NOT NULL,
-    "plugin_version" "text" NOT NULL,
-    "policy_version" integer NOT NULL,
-    "signature" "text" NOT NULL,
-    "recipe" "text" NOT NULL,
-    "active" boolean DEFAULT true NOT NULL,
-    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
-    "updated_at" timestamp with time zone DEFAULT "now"() NOT NULL
+                                   "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+                                   "public_key" "text" NOT NULL,
+                                   "plugin_id" "plugin_id" NOT NULL,
+                                   "plugin_version" "text" NOT NULL,
+                                   "policy_version" integer NOT NULL,
+                                   "signature" "text" NOT NULL,
+                                   "recipe" "text" NOT NULL,
+                                   "active" boolean DEFAULT true NOT NULL,
+                                   "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+                                   "updated_at" timestamp with time zone DEFAULT "now"() NOT NULL
 );
 
 CREATE TABLE "time_triggers" (
-    "id" integer NOT NULL,
-    "policy_id" "uuid" NOT NULL,
-    "cron_expression" "text" NOT NULL,
-    "start_time" timestamp without time zone NOT NULL,
-    "end_time" timestamp without time zone,
-    "frequency" integer NOT NULL,
-    "interval" integer NOT NULL,
-    "last_execution" timestamp without time zone,
-    "status" "trigger_status" NOT NULL,
-    "created_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+                                 "id" integer NOT NULL,
+                                 "policy_id" "uuid" NOT NULL,
+                                 "cron_expression" "text" NOT NULL,
+                                 "start_time" timestamp without time zone NOT NULL,
+                                 "end_time" timestamp without time zone,
+                                 "frequency" integer NOT NULL,
+                                 "interval" integer NOT NULL,
+                                 "last_execution" timestamp without time zone,
+                                 "status" "trigger_status" NOT NULL,
+                                 "created_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE SEQUENCE "time_triggers_id_seq"
@@ -68,31 +68,33 @@ CREATE SEQUENCE "time_triggers_id_seq"
 ALTER SEQUENCE "time_triggers_id_seq" OWNED BY "public"."time_triggers"."id";
 
 CREATE TABLE "transaction_history" (
-    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
-    "policy_id" "uuid" NOT NULL,
-    "tx_body" "text" NOT NULL,
-    "tx_hash" "text" NOT NULL,
-    "status" "transaction_status" NOT NULL,
-    "created_at" timestamp with time zone DEFAULT "now"(),
-    "updated_at" timestamp with time zone DEFAULT "now"(),
-    "metadata" "jsonb",
-    "error_message" "text"
+                                       "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+                                       "policy_id" "uuid" NOT NULL,
+                                       "tx_body" "text" NOT NULL,
+                                       "tx_hash" "text" NOT NULL,
+                                       "status" "transaction_status" NOT NULL,
+                                       "created_at" timestamp with time zone DEFAULT "now"(),
+                                       "updated_at" timestamp with time zone DEFAULT "now"(),
+                                       "metadata" "jsonb",
+                                       "error_message" "text"
 );
 
 CREATE TABLE "tx_indexer" (
-    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
-    "plugin_id" character varying(255) NOT NULL,
-    "tx_hash" character varying(255),
-    "chain_id" integer NOT NULL,
-    "policy_id" "uuid" NOT NULL,
-    "from_public_key" character varying(255) NOT NULL,
-    "proposed_tx_hex" "text" NOT NULL,
-    "status" "tx_indexer_status" DEFAULT 'PROPOSED'::"public"."tx_indexer_status" NOT NULL,
-    "status_onchain" "tx_indexer_status_onchain",
-    "lost" boolean DEFAULT false NOT NULL,
-    "broadcasted_at" timestamp without time zone,
-    "created_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    "updated_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+                              "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+                              "plugin_id" character varying(255) NOT NULL,
+                              "tx_hash" character varying(255),
+                              "chain_id" integer NOT NULL,
+                              "policy_id" "uuid" NOT NULL,
+                              "token_id" character varying(255) NOT NULL,
+                              "from_public_key" character varying(255) NOT NULL,
+                              "to_public_key" character varying(255) NOT NULL,
+                              "proposed_tx_hex" "text" NOT NULL,
+                              "status" "tx_indexer_status" DEFAULT 'PROPOSED'::"public"."tx_indexer_status" NOT NULL,
+                              "status_onchain" "tx_indexer_status_onchain",
+                              "lost" boolean DEFAULT false NOT NULL,
+                              "broadcasted_at" timestamp without time zone,
+                              "created_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                              "updated_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 ALTER TABLE ONLY "time_triggers" ALTER COLUMN "id" SET DEFAULT "nextval"('"public"."time_triggers_id_seq"'::"regclass");
@@ -127,6 +129,8 @@ CREATE INDEX "idx_transaction_history_policy_id" ON "transaction_history" USING 
 CREATE INDEX "idx_transaction_history_status" ON "transaction_history" USING "btree" ("status");
 
 CREATE INDEX "idx_transaction_history_tx_hash" ON "transaction_history" USING "btree" ("tx_hash");
+
+CREATE INDEX "idx_tx_indexer_key" ON "tx_indexer" USING "btree" ("chain_id", "plugin_id", "policy_id", "token_id", "to_public_key", "created_at");
 
 CREATE INDEX "idx_tx_indexer_status_onchain_lost" ON "tx_indexer" USING "btree" ("status_onchain", "lost");
 
